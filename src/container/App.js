2eea3aa515443d2a2016706ff7794334456f86a8
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2019-04-13 18:15:02 -0300

    Ultimas Alterações

--- src/container/App.js
+++ src/container/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Title from '../components/Title';
 import Square from '../components/Square';
 import Button from '../components/Button';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
