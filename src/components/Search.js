91a876d8c734c31a045ede73c70be72b357ec9ac
Marcio Rodrigues <marcio.silva@CLUBRNB1098>
2019-08-15 09:15:53 -0300

    refactor(search): get requests from server using axios
    Co-authored-by: Marcio Mafideju <mafideju@outlook.com>

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 export default function Search() {
   return (
@@ -7,6 +8,25 @@ export default function Search() {
         type="search"
         placeholder="Digite o Nome do Usuário"
         style={{ width: '20rem', height: '2rem', margin: '1rem 0' }}
+        onChange={e => {
+          const value = e.target.value;
+          axios
+            .get(`https://api.github.com/users/${value}`)
+            .then(result => {
+             console.log(result)
+            })
+        }}
+        // onKeyUp={e => {
+        //   const value = e.target.value;
+        //   const keyCode = e.which || e.keyCode;
+        //   if (keyCode === 13) {
+        //     axios
+        //       .get(`https://api.github.com/users/${value}`)
+        //       .then(result => {
+        //         console.log(result)
+        //       })
+        //   }
+        // }}
       />
     </div>
   )
